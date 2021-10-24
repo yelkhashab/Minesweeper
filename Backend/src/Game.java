e73c3304f0b6de5738fff02c2fec71e2497d6003
Youssef El Khashab <youssef_khashab@hotmail.com>
2021-10-24 10:27:50 -0700

    Adjacent Cells Method

--- Backend/src/Game.java
+++ Backend/src/Game.java
@@ -3,18 +3,26 @@ import java.io.IOException;
 import java.util.InputMismatchException;
 
 public class Game {
-    public static void main(String args[]) {
-        int gridWidth;
-        int gridHight;
-        int numMines;
+    int gridWidth;
+    int gridHight;
+    int numMines;
 
-        int[][] mines;
-        boolean[][] flags;
-        boolean[][] revealed;
+    int[][] mines;
+    boolean[][] flags;
+    boolean[][] revealed;
 
-        int main() {
+    int adjacentCells(int x, int y) {
+        int total = 0;
 
+        for (int i = -1; i < 1; i++){
+            for (int j = -1; j < 1; j++){
+                total += mines[x + i][y + j];
+            }
         }
+        return total;
+    }
+
+    public static void main(String args[]) {
 
     }
 
