7d726c38ec3b34c194f76b1840744339387e3da9
Youssef El Khashab <youssef_khashab@hotmail.com>
2021-10-24 10:21:56 -0700

    Game frame

---
+++ Backend/src/Game.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.InputMismatchException;
+
+public class Game {
+    public static void main(String args[]) {
+        int gridWidth;
+        int gridHight;
+        int numMines;
+
+        int[][] mines;
+        boolean[][] flags;
+        boolean[][] revealed;
+
+        int main() {
+
+        }
+
+    }
+
+}
